c3e443a268bf69a1af281def6f0473a4185d3ba3
nkosiphendule <ydomagazi0108@gmail.com>
2022-08-05 09:55:25 +0200

    added home.jsx

---
+++ src/components/Testimonal/Testimonals.jsx
@@ -0,0 +1,32 @@
+
+
+
+import React from 'react';
+import "./Testimonals.css"
+import { Container,Row, Col } from 'reactstrap';
+import Slider from "react-slick";
+
+import img from "../../assests/images/testimonal01.png"
+
+const Testimonals = () => {
+  return <section>
+    <Container>
+        <Row>
+            <Col lg="">
+                <div className="testimonal_wrapper d-flex justify-content-between">
+
+                    <div className="testimonal_img">
+                        <img src={img} alt="" />
+                    </div>
+
+                    <div className="testimonal_content">
+                        <h2>Our Students Voice</h2>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    </Container>
+  </section>
+};
+
+export default Testimonals;
