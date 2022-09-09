7b77f7868a885e463f7d359dba1e1b1a90c689cd
nkosiphendule <ydomagazi0108@gmail.com>
2022-09-09 08:33:24 +0200

    added fix it

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import "./newsletter.css";
-import { Container, Row, Col } from 'reactstrap';
+// import React from 'react';
+// import "./newsletter.css";
+// import { Container, Row, Col } from 'reactstrap';
 
 
-const Newsletter = () => {
-  return (
-    <section>
-        <Container className='newsletter'>
-            <Row>
-                <Col lg="12" className='text-center'>
-                    <h2 className='mb-3'>Our not have email</h2>
-                    <div className="add">
-                    <input type="text" placeholder='Email'/>
-                    <button className="button">add</button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    </section>
-  );
-};
+// const Newsletter = () => {
+//   return (
+//     <section>
+//         <Container className='newsletter'>
+//             <Row>
+//                 <Col lg="12" className='text-center'>
+//                     <h2 className='mb-3'>Our not have email</h2>
+//                     <div className="add">
+//                     <input type="text" placeholder='Email'/>
+//                     <button className="button">add</button>
+//                     </div>
+//                 </Col>
+//             </Row>
+//         </Container>
+//     </section>
+//   );
+// };
 
-export default Newsletter;
+// export default Newsletter;
